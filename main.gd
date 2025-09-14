edc1d62467f1d677d9048b8bd34ae9934bcf5643
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2025-09-14 16:28:53 +0330

    Create the HUD, and connect it to the Main scene. Remove old mobs.

--- main.gd
+++ main.gd
@@ -8,11 +8,15 @@ func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func _on_start_timer_timeout() -> void:
@@ -22,6 +26,7 @@ func _on_start_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_mob_timer_timeout() -> void:
@@ -40,8 +45,3 @@ func _on_mob_timer_timeout() -> void:
 	mob.linear_velocity = velocity.rotated(direction)
 
 	add_child(mob)
-
-
-func _ready() -> void:
-	# new_game()
-	pass
